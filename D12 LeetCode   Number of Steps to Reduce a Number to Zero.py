66a8f8001bcf5ad7162a3cc7944abc3b99f09be3
Saba Saeed <62014225+sabasaeed408@users.noreply.github.com>
2021-02-12 18:30:43 +0500

    Update D12 LeetCode   Number of Steps to Reduce a Number to Zero.py

--- D12 LeetCode   Number of Steps to Reduce a Number to Zero.py
+++ D12 LeetCode   Number of Steps to Reduce a Number to Zero.py
@@ -1,10 +1,10 @@
 class Solution:
     def numberOfSteps (self, num: int) -> int:
-        s=0
+        count=0
         while num>0:
             if num%2==0:
                 num=num//2
             else:
                 num=num-1
-            s=s+1
-        return s
+            count=count+1
+        return count
